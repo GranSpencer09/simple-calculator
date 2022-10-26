3446a384679feff969d3b21c0834d6f06935c56e
GranSpencer09 <Granspencer09@gmail.com>
2022-10-26 16:08:09 -0700

    initial setup, styling and html strucutre, getting started on js

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.getElementById("number");
+const operationsButtons = document.getElementById("operation");
+const equalsButton = document.getElementById("equals");
+const acButton = document.getElementById("ac");
+const delButton = document.getElementById("del");
+const prevOpText = document.getElementById("prev-op");
+const currOpText = document.getElementById("curr-op");
