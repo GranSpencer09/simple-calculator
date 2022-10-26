a2219f4543dc8e70b605ee8f46dbf973896ff81e
GranSpencer09 <Granspencer09@gmail.com>
2022-10-26 16:10:49 -0700

    psuedo code for calc functions

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+class Calculator {
+  constructor(prevOpText, currOpText) {
+    this.prevOpText = prevOpText;
+    this.currOpText = currOpText;
+  }
+  // what we need the calculator to do
+  // first, clear the prev and curr text
+  // add
+  // subtract
+  // divide
+  // multiply
+  // equal or solve
+}
+
 const numberButtons = document.getElementById("number");
 const operationsButtons = document.getElementById("operation");
-const equalsButton = document.getElementById("equals");
+const equalsButton = document.getElementById("equal");
 const acButton = document.getElementById("ac");
 const delButton = document.getElementById("del");
 const prevOpText = document.getElementById("prev-op");
