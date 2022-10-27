81e0f79fe9a5cfa8ecc47d55fe3c2a755d2b4f06
GranSpencer09 <Granspencer09@gmail.com>
2022-10-26 22:18:47 -0700

    fully functionaing calculator, added delete, chooseOperations, solve, getDisplayNumber helper function, and added to updateDisplay. i also added event listeners to all buttons

--- script.js
+++ script.js
@@ -12,23 +12,68 @@ class Calculator {
     this.operation = undefined;
   }
   // delete numbers
-  delete() {}
+  delete() {
+    this.currOp = this.currOp.toString().slice(0, -1);
+  }
   // add numbers
   appendNumber(number) {
-    // this.currOp = number;
+    // Prevent multiple decimal points
+    if (number === "." && this.currOp.includes(".")) return;
+    //console.log(this.currOp);
+
     this.currOp = this.currOp.toString() + number.toString();
   }
   // all of these could fit in one if else statement
-  chooseOperation(operation) {}
-  // add
-  // subtract
-  // divide
-  // multiply
-  // equal or solve
-  solve() {}
+  chooseOperation(operation) {
+    if (this.currOp === "") return;
+    if (this.prevOp !== "") {
+      this.solve();
+      console.log("running solve");
+    }
+    this.operation = operation;
+    this.prevOp = this.currOp;
+    this.currOp = "";
+  }
+  solve() {
+    let computation;
+    let curr = parseFloat(this.currOp);
+    let prev = parseFloat(this.prevOp);
+    if (prev === null || curr === null) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + curr;
+        break;
+      case "-":
+        computation = prev - curr;
+        break;
+      case "*":
+        computation = prev * curr;
+        break;
+      case "÷":
+        computation = prev / curr;
+        break;
+      default:
+        return;
+    }
+    this.currOp = computation;
+    this.operation = undefined;
+    this.prevOp = "";
+  }
+  getDisplayNumber(number) {
+    const floatNumber = parseFloat(number);
+    if (isNaN(floatNumber)) return "";
+    return floatNumber.toLocaleString("en");
+  }
 
   updateDisplay() {
-    this.currOpText.innerText = this.currOp;
+    this.currOpText.innerText = this.getDisplayNumber(this.currOp);
+    if (this.operation != null) {
+      this.prevOpText.innerText = `${this.getDisplayNumber(this.prevOp)} ${
+        this.operation
+      }`;
+    } else {
+      this.prevOpText.innerText = "";
+    }
   }
 }
 
@@ -41,11 +86,33 @@ const prevOpText = document.getElementById("prev-op");
 const currOpText = document.getElementById("curr-op");
 
 const calculator = new Calculator(prevOpText, currOpText);
-console.log(numberButtons.length);
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.appendNumber(button.innerText);
     calculator.updateDisplay();
   });
+});
+
+operationsButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    console.log(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+equalsButton.addEventListener("click", () => {
+  calculator.solve();
+  calculator.updateDisplay();
+});
+
+acButton.addEventListener("click", () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+delButton.addEventListener("click", () => {
+  calculator.delete();
+  calculator.updateDisplay();
 });
